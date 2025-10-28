43416a9e4dd5b99f1bb58da6abf57ff54284ab0e
Pedro Alves <101369810+pdroAlves77@users.noreply.github.com>
2025-10-27 22:22:22 -0300

    iniciando setup do app com fastify

--- apps/api/src/index.ts
+++
@@ -1,5 +0,0 @@
-import { defineAbilityFor, projectSchema } from '@saas/auth'
-
-const ability = defineAbilityFor({ role: 'MEMBER', id: 'user-id' })
-
-const project = projectSchema.parse({ id: 'project-id', ownerId: 'user-id' })
